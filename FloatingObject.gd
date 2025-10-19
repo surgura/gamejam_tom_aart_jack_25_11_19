b39dfb723d19f2f00776066b2bb5464c71df98e6
Eing00 <jutrasjack@gmail.com>
2025-10-19 18:44:47 +0200

    Audio changes

--- FloatingObject.gd
+++ FloatingObject.gd
@@ -4,6 +4,10 @@ class_name FloatingObject
 
 var velocity = Vector2(0,0)
 var lowFps:int = 0
+@onready var rocketSFX = $Rocket
+
+func _ready():
+	rocketSFX.play() 
 
 func _physics_process(delta) -> void:
 	velocity += GravitySystem.gravity_at(position) * delta
