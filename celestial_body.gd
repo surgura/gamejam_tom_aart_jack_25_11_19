25b683c4d1676f62f0b44334edff8ec04c506f52
Aart Stuurman <aartstuurman@hotmail.com>
2025-10-19 18:47:26 +0200

    camera and only changing size in pause mode

--- celestial_body.gd
+++ celestial_body.gd
@@ -24,7 +24,8 @@ func _ready():
 	connect("input_event", Callable(self, "_on_input_event"))
 
 func _on_input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		self.uniform_scale += 0.5
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
-		self.uniform_scale -= 0.5
+	if Gamestate.is_paused:
+		if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			self.uniform_scale += 0.5
+		if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+			self.uniform_scale -= 0.5
