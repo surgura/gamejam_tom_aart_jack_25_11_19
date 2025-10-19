737eff3671879b223e75f58ba0ffff55ac4557fa
Eing00 <jutrasjack@gmail.com>
2025-10-19 18:16:02 +0200

    :) animation change

--- FloatingObject.gd
+++ FloatingObject.gd
@@ -3,13 +3,12 @@ extends Node2D
 class_name FloatingObject
 
 var velocity = Vector2(0,0)
-
+var lowFps:int = 0
 
 func _physics_process(delta) -> void:
 	velocity += GravitySystem.gravity_at(position) * delta
 	position += velocity * delta
 	
-	if velocity != Vector2.ZERO:
-		# For 2D, use look_at with a direction vector
-		# The Vector2.UP is the 'up' direction for look_at, but you can use any vector
+	if (velocity != Vector2.ZERO) && (lowFps%10 == 0):
 		look_at(global_position + velocity)
+	lowFps += 1
