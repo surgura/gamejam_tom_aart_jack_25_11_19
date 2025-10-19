e4473e48a1b850b88e738ab89c683c1450d4eec2
Aart Stuurman <aartstuurman@hotmail.com>
2025-10-19 16:03:39 +0200

    added planets

--- GravitySource.gd
+++ GravitySource.gd
@@ -1,11 +1,11 @@
 class_name GravitySource
-extends Node
+extends Node2D
 
 @export var mass := 1.0
 
 var root_position: Vector2:
 	get:
-		return get_parent().global_position
+		return global_position
 
 func _enter_tree():
 	GravitySystem.register_source(self)
