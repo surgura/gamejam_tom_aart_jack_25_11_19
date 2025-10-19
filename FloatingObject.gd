46923800e5143c7567e3c1e947987ffbd25c1aa4
Eing00 <jutrasjack@gmail.com>
2025-10-19 17:42:53 +0200

    added sound and cool

--- FloatingObject.gd
+++ FloatingObject.gd
@@ -8,3 +8,8 @@ var velocity = Vector2(0,0)
 func _physics_process(delta) -> void:
 	velocity += GravitySystem.gravity_at(position) * delta
 	position += velocity * delta
+	
+	if velocity != Vector2.ZERO:
+		# For 2D, use look_at with a direction vector
+		# The Vector2.UP is the 'up' direction for look_at, but you can use any vector
+		look_at(global_position + velocity)
